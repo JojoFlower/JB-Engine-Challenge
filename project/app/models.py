5b37e505c0b6d731c1a56dde06ca3f8de6fcc9e2
Joël NGuesson <nguessonjoel@gmail.com>
2018-08-29 02:09:23 +0200

    add student and school models

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class School(models.Model):
+    name = models.CharField(
+        max_length=20,
+        verbose_name='School Name'
+    )
+    max_students = models.PositiveIntegerField(
+        verbose_name='Maximum Number of Students'
+    )
+
+
+class Student(models.Model):
+    first_name = models.CharField(
+        max_length=20,
+        verbose_name='First Name'
+    )
+    last_name = models.CharField(
+        max_length=20,
+        verbose_name='Last Name'
+    )
+    student_id = models.CharField(
+        max_length=20,
+        verbose_name='Student ID'
+    )
+    school = models.ForeignKey(
+        School,
+        on_delete=models.CASCADE,
+        verbose_name='Student School'
+    )
