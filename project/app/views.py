79fff310e583160d8dd506f8417f40206b6d1f1d
Joël NGuesson <nguessonjoel@gmail.com>
2018-08-29 04:09:32 +0200

    create views and add methods to get all objects

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import School, Student
+from .serializer import SchoolSerializer, StudentSerializer
 
-# Create your views here.
+
+class SchoolList(APIView):
+    def get(self, request):
+        schools = School.objects.all()
+        serializer = SchoolSerializer(schools, many=True)
+        return Response(serializer.data)
+
+
+class StudentList(APIView):
+    def get(self, request):
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
