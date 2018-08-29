f5276b31e80f4ad426e195972a94d85166fc5021
Joël NGuesson <nguessonjoel@gmail.com>
2018-08-29 02:37:08 +0200

    create serializers

---
+++ project/app/serializer.py
@@ -0,0 +1,21 @@
+from .models import School, Student
+from rest_framework import serializers
+
+
+class SchoolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = School
+        fields = (
+            'name',
+            'max_students'
+        )
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = (
+            'first_name',
+            'last_name',
+            'student_id'
+        )
